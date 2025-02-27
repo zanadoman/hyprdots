167ddf5da70ae0db022a1388fe0793d775b39964
zanadoman <domanseven@gmail.com>
2025-02-27 14:14:49 +0100

    changed install order

--- install.sh
+++ install.sh
@@ -12,6 +12,18 @@ pacman -Qdtq | sudo pacman -Rns -
 cp -r ./home/. "$HOME/"
 chsh -s /bin/fish
 
+# Android
+sudo archlinux-java set java-8-openjdk
+yes | sudo env PATH="$PATH" sdkmanager --licenses
+sudo env PATH="$PATH" sdkmanager platform-tools emulator 'system-images;android-34;google_apis_playstore;x86_64'
+avdmanager create avd --name android34 --package 'system-images;android-34;google_apis_playstore;x86_64' --device pixel
+echo 'hw.keyboard = yes' >> "$HOME/.android/avd/android34.avd/config.ini"
+sudo archlinux-java set java-17-openjdk
+
+# C#
+sudo dotnet workload update
+dotnet new install Avalonia.Templates
+
 # MariaDB
 sudo mariadb-install-db --user=mysql --basedir=/usr/ --datadir=/var/lib/mysql/
 sudo systemctl start mariadb.service
@@ -20,18 +32,6 @@ sudo mariadb-secure-installation
 # Rust
 rustup default stable
 rustup target add wasm32-unknown-unknown
-
-# C#
-sudo dotnet workload update
-dotnet new install Avalonia.Templates
-
-# Android
-sudo archlinux-java set java-8-openjdk
-yes | sudo env PATH="$PATH" sdkmanager --licenses
-sudo env PATH="$PATH" sdkmanager platform-tools emulator 'system-images;android-34;google_apis_playstore;x86_64'
-avdmanager create avd --name android34 --package 'system-images;android-34;google_apis_playstore;x86_64' --device pixel
-echo 'hw.keyboard = yes' >> "$HOME/.android/avd/android34.avd/config.ini"
-sudo archlinux-java set java-17-openjdk
 
 # Folders
 mkdir "$HOME/Desktop/"
