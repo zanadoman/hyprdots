2dfd6fcac0d9dfcad711d80dfb6b7694bcc2fb1a
zanadoman <domanseven@gmail.com>
2025-05-26 15:26:45 +0200

    clean explicit sh

--- explicit.sh
+++ explicit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cat pacman.txt aur.txt | while read package line; do
-    echo -e 'n\n' | sudo pacman -Rns $package
+cat pacman.txt aur.txt | while read package; do
+    echo -e n\n | sudo pacman -Rns $package
 done 2>&1 | grep -Po ':: removing \K[^ ]+'
