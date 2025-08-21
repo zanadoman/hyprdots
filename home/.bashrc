8101112ae678362e30a41ff4975273b9818e6146
zanadoman <domanseven@gmail.com>
2025-08-21 11:25:26 +0200

    simpler shell config

--- home/.bashrc
+++ home/.bashrc
@@ -1,7 +1,6 @@
 [[ $- != *i* ]] && return
 export LIBRARY_PATH="/usr/local/lib/:$LIBRARY_PATH"
 export LD_LIBRARY_PATH="/usr/local/lib/:$LD_LIBRARY_PATH"
-export PKG_CONFIG_SYSROOT_DIR=/usr/x86_64-w64-mingw32
 export ANDROID_SDK_ROOT=/opt/android-sdk
 export ANDROID_NDK_HOME=/opt/android-ndk
 export PATH="$HOME/.local/bin/:$HOME/.cargo/bin/:$PATH:$ANDROID_SDK_ROOT/tools/bin/:$ANDROID_SDK_ROOT/platform-tools/:$ANDROID_SDK_ROOT/emulator/:$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/"
@@ -17,58 +16,13 @@ alias clear='clear && fastfetch'
 alias hyprland='[ $XDG_SESSION_TYPE = tty ] && hyprland && clear'
 eval "$(starship init bash)"
 eval "$(zoxide init bash --cmd cd)"
+eval "$(fzf --bash)"
 clear
-
-ff () {
-    if [ $# -eq 0 ]; then
-        root=./
-    else
-        root=$@
-    fi
-    find $root -mindepth 1 -type f | fzf
-}
-
-frg () {
-    if [ $# -eq 0 ]; then
-        root=./
-    else
-        root=$@
-    fi
-    selected=$(cat $(find $root -mindepth 1 -type f) | fzf)
-    if [ -n "$selected" ]; then
-        rg -.lF "$selected"
-    fi
-}
-
-fd () {
-    if [ $# -eq 0 ]; then
-        root=./
-    else
-        root=$@
-    fi
-    find $root -mindepth 1 -type d | fzf
-}
-
-fcd () {
-    if [ $# -eq 0 ]; then
-        root=./
-    else
-        root=$@
-    fi
-    selected=$(find $root -mindepth 1 | fzf)
-    if [ -n "$selected" ]; then
-        if [ -d "$selected" ]; then
-            cd "$selected"
-        else
-            cd "$(dirname "$selected")"
-        fi
-    fi
-}
 
 tmux () {
     if [ 0 -lt $# ]; then
         command tmux "$@"
     else
-        command tmux attach-session &>/dev/null || command tmux new-session -s "$(basename "$PWD")"
+        command tmux attach-session -t "$(basename "$PWD")" &>/dev/null || command tmux new-session -s "$(basename "$PWD")"
     fi
 }
--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,7 +2,6 @@ not status is-interactive && return
 set -U fish_greeting
 set -x LIBRARY_PATH "/usr/local/lib/:$LIBRARY_PATH"
 set -x LD_LIBRARY_PATH "/usr/local/lib/:$LD_LIBRARY_PATH"
-set -x PKG_CONFIG_SYSROOT_DIR /usr/x86_64-w64-mingw32
 set -x ANDROID_SDK_ROOT /opt/android-sdk
 set -x ANDROID_NDK_HOME /opt/android-ndk
 set -x PATH "$HOME/.local/bin/:$HOME/.cargo/bin/:$PATH:$ANDROID_SDK_ROOT/tools/bin/:$ANDROID_SDK_ROOT/platform-tools/:$ANDROID_SDK_ROOT/emulator/:$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/"
@@ -18,58 +17,13 @@ alias clear='clear && fastfetch'
 alias hyprland='test $XDG_SESSION_TYPE = tty && command hyprland && clear'
 starship init fish | source
 zoxide init fish --cmd cd | source
+fzf --fish | source
 clear
-
-function ff
-    if test (count $argv) -eq 0
-        set root ./
-    else
-        set root $argv
-    end
-    find $root -mindepth 1 -type f | fzf
-end
-
-function frg
-    if test (count $argv) -eq 0
-        set root ./
-    else
-        set root $argv
-    end
-    set selected (cat (find $root -mindepth 1 -type f) | fzf)
-    if test -n "$selected"
-        rg -.lF $selected
-    end
-end
-
-function fd
-    if test (count $argv) -eq 0
-        set root ./
-    else
-        set root $argv
-    end
-    find $root -mindepth 1 -type d | fzf
-end
-
-function fcd
-    if test (count $argv) -eq 0
-        set root ./
-    else
-        set root $argv
-    end
-    set selected (find $root -mindepth 1 | fzf)
-    if test -n "$selected"
-        if test -d $selected
-            cd $selected
-        else
-            cd (dirname $selected)
-        end
-    end
-end
 
 function tmux
     if test 0 -lt (count $argv)
         command tmux $argv
     else
-        command tmux attach-session &>/dev/null || command tmux new-session -s (basename $PWD)
+        command tmux attach-session -t (basename $PWD) &>/dev/null || command tmux new-session -s (basename $PWD)
     end
 end
