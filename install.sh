e115e87d45e74c69d734de7f75990841ade71004
zanadoman <domanseven@gmail.com>
2025-10-31 06:54:38 +0100

    rust default toolchain stable

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ sudo env JAVA_HOME=/usr/lib/jvm/java-8-openjdk /opt/android-sdk/tools/bin/sdkman
 
 # Rust
 rustup default stable
-rustup default nightly
+rustup toolchain install nightly
 rustup target add aarch64-linux-android
 rustup target add x86_64-linux-android
 rustup target add x86_64-pc-windows-gnu
